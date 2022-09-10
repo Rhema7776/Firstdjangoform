a5f045080b2af411bee29592ef2822f774e1abfe
Rhema7776 <rhemachukwumah7@gmail.com>
2022-09-10 13:40:16 +0100

    cleaned import djdburl

--- Forms/settings.py
+++ Forms/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,8 +84,8 @@ DATABASES = {
         
     }
 }
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
