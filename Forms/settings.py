cfc92e38227e1aede192e10f8b28433b47d47a46
Rhema7776 <rhemachukwumah7@gmail.com>
2022-09-10 13:29:22 +0100

    default db

--- Forms/settings.py
+++ Forms/settings.py
@@ -79,9 +79,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
+        
     }
 }
-
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
